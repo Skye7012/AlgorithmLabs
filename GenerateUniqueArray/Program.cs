a3c64d6569aec828cb3d5ac7465a9261c1b32426
Skye7012 <skye7012@gmail.com>
2021-09-08 00:25:14 +0300

    Completed Unique Generation

--- GenerateUniqueArray/Program.cs
+++ GenerateUniqueArray/Program.cs
@@ -7,15 +7,28 @@ namespace GenerateUniqueArray
 	static class Program
 	{
 		private const int length = 10000000; //10 000 000
+		private const int tries = 10;
 		private static Random rnd = new Random();
-		private static int count = 0;
 		private static void Main(string[] args)
 		{
-			int[] array = GenerateUniqueArray();
+			var was = DateTime.Now;
+
+			int[] maxShifts = new int[tries];
+
+			for(int i = 0; i < tries; i++)
+			{
+				maxShifts[i] = GetShiftsOnMax();
+				Console.WriteLine($"Shifts in {i+1}th try = {maxShifts[i]}\n");
+			}
+
+			Console.WriteLine($"\n\nAverage Shifts = {maxShifts.Average()}");
+
+			Console.WriteLine(DateTime.Now - was);
 		}
 		private static int[] GenerateUniqueArray()
 		{
 			HashSet<int> res = new HashSet<int>(length);
+			int count = 0;
 
 			for (int i = 0; i < length; i++)
 			{
@@ -24,13 +37,31 @@ namespace GenerateUniqueArray
 				while(res.Count != count)
 				{
 					res.Add(rnd.Next());
-					Console.WriteLine("aaa" + count + "\n" + i);
 				}
 			}
 
-			Console.WriteLine("Generate Done \n\n\n " + count);
+			Console.WriteLine("Generate Done");
+			Console.WriteLine("Array Length = " + count);
 
 			return res.ToArray();
 		}
+		private static int GetShiftsOnMax()
+		{
+			int max = 0;
+			int countMaxChanges = 0;
+			var array = GenerateUniqueArray();
+
+			foreach (var arr in array)
+			{
+				if(arr > max)
+				{
+					max = arr;
+					countMaxChanges++;
+				}
+			}
+
+			return countMaxChanges;
+		}
+
 	}
 }
