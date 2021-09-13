cfac38846add29b52377ae3236b223cb302995d9
Skye7012 <skye7012@gmail.com>
2021-09-13 18:10:11 +0300

    added mapping, finded sequence

--- LongestCommonSubsequenceLab/Program.cs
+++ LongestCommonSubsequenceLab/Program.cs
@@ -29,8 +29,35 @@ namespace LongestCommonSubsequenceLab
 						}
 					}
 				}
-
+				Console.WriteLine(matrix[m, n]);
 				Print(matrix);
+
+
+				//int i = m;
+				//int j = n;
+				var now = matrix[m, n];
+				string s = "";
+
+				for (int i = m, j = n; j != 0 || i != 0;)
+				{
+					if (j == 0 || i == 0)
+						break;
+					var first = matrix[i - 1, j];
+					var second = matrix[i, j - 1];
+					if (matrix[i, j] > first && matrix[i, j] > second)
+					{
+						s += s1[i - 1];//
+						i--;
+						j--;
+					}
+					else if (first >= second)
+						i--;
+					else
+						j--;
+				}
+
+				Console.WriteLine(s);
+
 			}
 		}
 
