821ea6959701a1a212b047c9667f4dfbabbf23e1
Skye7012 <skye7012@gmail.com>
2021-10-04 20:56:25 +0300

    Added LongestIncreasingSubsequenceLab

---
+++ LongestIncreasingSubsequenceLab/LongestIncreasingSubsequence.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LongestIncreasingSubsequenceLab
+{
+	public class LongestIncreasingSubsequence
+	{
+		int[] _numbers;
+		int[] _maxSequenceLength;
+
+		public LongestIncreasingSubsequence(int[] numbers)
+		{
+			_numbers = numbers;
+			_maxSequenceLength = Enumerable.Repeat(1, _numbers.Length).ToArray();
+
+		}
+
+		public int GetLongestIncreasingSubsequenceLength()
+		{
+			for (int i = 1; i < _numbers.Length; i++)
+			{
+				for (int j = 0; j < i; j++)
+				{
+					if (_numbers[i] > _numbers[j]
+						&& _maxSequenceLength[i] <= _maxSequenceLength[j])
+						_maxSequenceLength[i]++;
+				}
+			}
+
+			return _maxSequenceLength.Max();
+		}
+
+		public int[] GetLongestIncreasingSubsequence()
+		{
+			int max = GetLongestIncreasingSubsequenceLength();
+			var indexOfMax = _maxSequenceLength.ToList().
+				IndexOf(max);
+
+			var res = new List<int>() { _numbers[indexOfMax]};
+
+
+			for (int j = indexOfMax, k = j - 1; k >= 0; k--)
+			{
+				if (_maxSequenceLength[j] == _maxSequenceLength[k] + 1)
+				{
+					res.Add(_numbers[k]);
+					j = k;
+				}
+			}
+
+
+			return res.ToArray();
+		}
+	}
+}
---
+++ LongestIncreasingSubsequenceLab/Program.cs
@@ -0,0 +1,21 @@
+﻿using MyTimer;
+using System;
+using System.Linq;
+
+namespace LongestIncreasingSubsequenceLab
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			using (var timer = new Timer())
+			{
+				var numbers = new int[] { 5, 10, 6, 12, 3, 24, 7, 8 };
+				var lis = new LongestIncreasingSubsequence(numbers);
+				Console.WriteLine("Max Length: " + lis.GetLongestIncreasingSubsequenceLength());
+				Console.WriteLine("SubSequence: ");
+				Array.ForEach(lis.GetLongestIncreasingSubsequence(), Console.WriteLine);
+			}
+		}
+	}
+}
