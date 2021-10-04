7fc7d0131147aa4394094c653ab1f5e0199e9d56
Skye7012 <skye7012@gmail.com>
2021-10-04 21:02:13 +0300

    Added LongestIncreasingSubsequenceTests

---
+++ tests/UnitTests/LongestIncreasingSubsequenceTests/GetLongestIncreasingSubsequenceTests.cs
@@ -0,0 +1,22 @@
+using LongestCommonSubsequenceLab;
+using LongestIncreasingSubsequenceLab;
+using Xunit;
+
+namespace UnitTests.LongestIncreasingSubsequenceTests
+{
+	public class GetLongestIncreasingSubsequenceTests
+	{
+		[Fact]
+		public void SendSequence_ShouldReturnLongestIncreasingSubsequence()
+		{
+			var numbers = new int[] { 5, 10, 6, 12, 3, 24, 7, 8 };
+			var lis = new LongestIncreasingSubsequence(numbers);
+
+			var expectedSubseq = new int[] { 24, 12, 6, 5 };
+
+			var actualSubseq = lis.GetLongestIncreasingSubsequence();
+
+			Assert.Equal(expectedSubseq, actualSubseq);
+		}
+	}
+}
