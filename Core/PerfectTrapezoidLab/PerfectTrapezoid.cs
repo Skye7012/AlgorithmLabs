c4994e8d09c7e8657d124b34f8ebe338abf5bb75
Skye7012 <skye7012@gmail.com>
2021-11-03 00:30:27 +0300

    PerfectTrapezoid completed

---
+++ Core/PerfectTrapezoidLab/PerfectTrapezoid.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Linq;
+using MyLib.EulerCycleLib;
+
+namespace PerfectTrapezoidLab
+{
+	public class PerfectTrapezoid
+	{
+		const string _path = "..\\..\\..\\Task\\";
+
+		HashSet<(int, int, int)> _trapezoids = new HashSet<(int, int, int)>();
+
+		int _amount;
+
+		public PerfectTrapezoid(int num)
+		{
+			ReadFile(num);
+		}
+
+		public List<int> Solve()
+		{
+			var eulerCycle = new EulerCycle(_trapezoids);
+			var res = eulerCycle.Solve(FindStartTrapezoid());
+
+			return res.Select(x => x.rib).ToList();
+		}
+
+		int FindStartTrapezoid()
+			=> _trapezoids.Min(x => Math.Abs(x.Item1));
+
+
+		public void ReadFile(int num)
+		{
+			string path = _path + num;
+
+			var rawText = File.ReadAllLines(path);
+
+			_amount = Convert.ToInt32(rawText[0]);
+
+			for (int i = 0; i < _amount; i++)
+			{
+				_trapezoids.Add(ConvertStringToTrapezoid(rawText[i + 1], i + 1));
+			}
+		}
+
+		(int, int, int) ConvertStringToTrapezoid(string stringTuple, int num)
+		{
+			var elements = stringTuple.Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
+
+			int leftAngle = elements[0];
+			int rightAngle = elements[1] - elements[2];
+
+			return (leftAngle, num, rightAngle);
+		}
+	}
+}
---
+++ Core/PerfectTrapezoidLab/Program.cs
@@ -0,0 +1,32 @@
+﻿using MyTimer;
+using System;
+using System.IO;
+
+namespace PerfectTrapezoidLab
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			using Timer timer = new Timer();
+
+			for (int i = 1; i <= 9; i++)
+			{
+				try
+				{
+					var res = string.Join(", ", new PerfectTrapezoid(i).Solve());
+
+					File.WriteAllText("..\\..\\..\\Solutions\\" + i + ".txt", res);
+
+					Console.WriteLine($"{i}ая задача успешно решена");
+				}
+				catch (Exception ex)
+				{
+					string message = $"В {i}ой задаче возникла ошибка : '{ex.Message}'";
+					Console.WriteLine(message);
+					File.WriteAllText("..\\..\\..\\Solutions\\" + i + ".txt", message);
+				}
+			}
+		}
+	}
+}
